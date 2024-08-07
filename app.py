2e4a5e88f7bb7461edcfad48f2fd023ee1b26829
Manikandan Alagu <Manikandan-Alagu@users.noreply.huggingface.co>
2024-08-07 08:54:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,39 @@
-
 import os
 import gradio as gr
 import google.generativeai as genai
 
+# Configure API key for Gemini
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", None)
 genai.configure(api_key=GEMINI_API_KEY)
 
-def model(input):
-    
-    # Create the model
-    generation_config = {
-      "temperature": 1,
-      "top_p": 0.95,
-      "top_k": 64,
-      "max_output_tokens": 8192,
-      "response_mime_type": "text/plain",
-    }
+# Create the model and chat session once
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
 
-    model = genai.GenerativeModel(
-      model_name="gemini-1.5-flash",
-      generation_config=generation_config,
-      # safety_settings = Adjust safety settings
-      # See https://ai.google.dev/gemini-api/docs/safety-settings
-      system_instruction="give the response in friendly tone",
-    )
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+    system_instruction="give the response in a friendly tone",
+)
 
-    chat_session = model.start_chat(
-      history=[
-          
-      ]
-    )
+chat_session = model.start_chat(
+    history=[]
+)
 
+def chat(input):
     response = chat_session.send_message(input)
-
     return response.text
 
-
-
+# Create Gradio interface
 inputs = gr.Textbox(placeholder="Let Chat")
 outputs = gr.Textbox()
-demo  = gr.Interface(model, inputs, outputs)
+demo = gr.Interface(fn=chat, inputs=inputs, outputs=outputs)
 
+# Launch the Gradio interface
 if __name__ == "__main__":
     demo.launch()
