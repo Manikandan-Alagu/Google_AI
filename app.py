7a2aaa0237ea827b11b4e5ee2a623792a2c510de
Manikandan Alagu <Manikandan-Alagu@users.noreply.huggingface.co>
2024-08-07 04:13:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 
 import os
 import gradio as gr
-
 import google.generativeai as genai
 
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", None)
 genai.configure(api_key=GEMINI_API_KEY)
 
-# Create the model
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
+def model(input):
+    
+    # Create the model
+    generation_config = {
+      "temperature": 1,
+      "top_p": 0.95,
+      "top_k": 64,
+      "max_output_tokens": 8192,
+      "response_mime_type": "text/plain",
+    }
 
-model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
-  generation_config=generation_config,
-  # safety_settings = Adjust safety settings
-  # See https://ai.google.dev/gemini-api/docs/safety-settings
-  system_instruction="give the response in friendly tone",
-)
+    model = genai.GenerativeModel(
+      model_name="gemini-1.5-flash",
+      generation_config=generation_config,
+      # safety_settings = Adjust safety settings
+      # See https://ai.google.dev/gemini-api/docs/safety-settings
+      system_instruction="give the response in friendly tone",
+    )
 
-chat_session = model.start_chat(
-  history=[
-      
-  ]
-)
+    chat_session = model.start_chat(
+      history=[
+          
+      ]
+    )
 
-response = chat_session.send_message("INSERT_INPUT_HERE")
+    response = chat_session.send_message(input)
 
-print(response.text)
+    return response.text
+
+
+
+demo = gr.Interface(fn=model, inputs="textbox", outputs="textbox")
+
+if __name__ == "__main__":
+    demo.launch()
